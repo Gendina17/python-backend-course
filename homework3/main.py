bc746567b1db5fd8a07b623fe4d0b9be82e161b9
Gendina Nina <ng@level.travel>
2021-10-24 18:15:43 +0300

    start homework3 and rename homework1

---
+++ homework3/main.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+import json
+import sys
+
+def app(environ, start_response):
+	data = {'time': str(datetime.now().time()), 'url': environ["HTTP_HOST"]}
+	data = json.dumps(data).encode('utf-8')
+	start_response("200 OK", [
+		("Content-Type", "application/json"),
+		("Content-Length", str(len(data)))
+	])
+	return iter([data])
