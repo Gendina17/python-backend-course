c064a316430d7ecc65b0e1e370066a8cbd00b4f5
Gendina Nina <ng@level.travel>
2021-10-24 22:23:03 +0300

    add test file

--- homework3/main.py
+++ homework3/main.py
@@ -2,10 +2,11 @@ from datetime import datetime
 import json
 
 def app(environ, start_response):
-	data = {'time': str(datetime.now().time()), 'url': environ["HTTP_HOST"]}
+	data = {'time': datetime.now().strftime("%H:%M:%S"),
+			'url': environ["HTTP_HOST"] + environ["RAW_URI"]}
 	data = json.dumps(data).encode('utf-8')
 	start_response("200 OK", [
 		("Content-Type", "application/json"),
 		("Content-Length", str(len(data)))
 	])
-	return iter([data])
+	return [data]
