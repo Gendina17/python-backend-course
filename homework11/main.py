7087ecb77339dc6e645113cabf141cc34ea123bf
Gendina Nina <ng@level.travel>
2021-12-15 21:50:46 +0300

    homework11

--- homework11/main.py
+++ homework11/main.py
@@ -4,28 +4,21 @@ import sys
 def calculating_distance(string, pattern):
     if string == pattern:
         return 0
-    string = ' ' + string
-    pattern = ' ' + pattern
-    width = len(string)
-    height = len(pattern)
-    matrix = [[0 for j in range(height)] for i in range(width)]
-    matrix[0] = [i for i in range(height)]
-    for j in range(width):
-        matrix[j][0] = j
-    print(matrix)
 
-    for string_index in range(1, width):
-        for pattern_index in range(1, height):
-            additional_value = 0 if string[string_index] == pattern[pattern_index] else 1
-            matrix[string_index][pattern_index] = sorted([
+    matrix = [[0 for j in range(len(pattern)+1)] for i in range(len(string)+1)]
+    matrix[0] = [i for i in range(len(pattern)+1)]
+    for j in range(len(string)+1):
+        matrix[j][0] = j
+
+    for string_index in range(1, len(string)+1):
+        for pattern_index in range(1, len(pattern)+1):
+            additional_value = 0 if string[string_index-1] == pattern[pattern_index-1] else 1
+            matrix[string_index][pattern_index] = min([
                 matrix[string_index - 1][pattern_index] + 1, matrix[string_index][pattern_index - 1] + 1,
                 matrix[string_index - 1][pattern_index - 1] + additional_value
-            ])[0]
-    print(matrix)
-    print(matrix[-1][-1])
+            ])
     return matrix[-1][-1]
 
 
 if __name__ == '__main__':
-    # calculating_distance(sys.argv[1], sys.argv[2])
-    calculating_distance('машка', 'миша')
+    calculating_distance(sys.argv[1], sys.argv[2])
--- homework11/test.py
+++ homework11/test.py
@@ -12,18 +12,19 @@ class TestCalculatingDistance(unittest.TestCase):
         str = self.random_str()
         self.assertEqual(0, calculating_distance(str, str))
 
-    def test_1end(self):
+    def test_addition_to_the_second_word(self):
         str = self.random_str()
-        self.assertEqual(1, calculating_distance(str, str+'w'))
+        self.assertEqual(1, calculating_distance(str, str + 'w'))
         self.assertEqual(1, calculating_distance(str, 'w' + str))
         self.assertEqual(3, calculating_distance(str, str + 'wer'))
         self.assertEqual(3, calculating_distance(str, 'wer' + str))
-    def test_1_beg(self):
+
+    def test_addition_to_the_first_word(self):
         str = self.random_str()
         self.assertEqual(1, calculating_distance(str + 'w', str))
         self.assertEqual(1, calculating_distance('w' + str, str))
 
-    def test_chan(self):
+    def test_adding_to_the_middle(self):
         str = 'qqqqq'
         str2 = 'qqwqq'
         self.assertEqual(1, calculating_distance(str, str2))
@@ -34,22 +35,27 @@ class TestCalculatingDistance(unittest.TestCase):
         str2 = 'qqweq'
         self.assertEqual(2, calculating_distance(str, str2))
 
-    def test_comb(self):
-        a = 'mama'
-        b = 'mom'
-        c = 'd'
-        e = 'ded'
-        f = 'pkokp'
-        t = 'kok'
-        u = 'qwer'
-        p = 'wer'
-        o = 'qwerty'
-        x = 'awert'
-        self.assertEqual(2, calculating_distance(a, b))
-        self.assertEqual(2, calculating_distance(c, e))
-        self.assertEqual(2, calculating_distance(f, t))
-        self.assertEqual(1, calculating_distance(u, p))
-        self.assertEqual(2, calculating_distance(o, x))
+    def test_different_combinations(self):
+        first = 'mama'
+        second = 'mom'
+        self.assertEqual(2, calculating_distance(first, second))
+        first = 'd'
+        second = 'ded'
+        self.assertEqual(2, calculating_distance(first, second))
+        first = 'pkokp'
+        second = 'kok'
+        self.assertEqual(2, calculating_distance(first, second))
+        first = 'qwer'
+        second = 'wer'
+        self.assertEqual(1, calculating_distance(first, second))
+        first = 'qwerty'
+        second = 'awert'
+        self.assertEqual(2, calculating_distance(first, second))
+
+    def test_empty_string(self):
+        str = self.random_str()
+        self.assertEqual(len(str), calculating_distance('', str))
+
 
 if __name__ == '__main__':
     unittest.main()
